580fa057dbb96032f1a62e424a385af5a1208bc9
optimusblack <emilakhani@gmail.com>
2020-03-20 16:31:21 +0500

    NT upload and saving on disk

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var request = require('request');
 var path = require('path');
+const fileupload = require('express-fileupload');
 
 var app = express();
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(fileupload());
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname+'/home.html'));
-})
+});
 
-app.get('/result', function(req, res){
+app.post('/result', function(req, res){
+    var NTFile = req.files.inputNT;
+    var filePath = __dirname + '/' + 'NTinput.nt';
+    
+    NTFile.mv(filePath, (error) => {
+        if (error){
+            console.log(error);
+        }
+    });
+    
     res.sendFile(path.join(__dirname+'/HIN.svg'));
-})
+});
 
 var server = app.listen(8080, function(req,res){
     console.log("server started");
